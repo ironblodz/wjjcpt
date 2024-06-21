36b0643de8a1fe1e20012af0084e67cc449c687e
ironblodz <naylestorm12@gmail.com>
2024-06-21 08:37:50 +0100

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
     <title>@yield('title')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
